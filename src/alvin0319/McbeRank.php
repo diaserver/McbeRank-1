90375c9da4942d1536554471f9bcd4d8701454b0
NLOG <sorisem4106@naver.com>
2018-11-03 19:10:08 +0900

    yaml_parse to json_decode

--- src/alvin0319/McbeRank.php
+++ src/alvin0319/McbeRank.php
@@ -16,9 +16,9 @@ class McbeRank extends PluginBase implements Listener{
             'port' => $this->getServer()->getPort()
         ]);
         $this->db = $this->config->getAll();
-        $url = yaml_parse($this->getServerRank());
+        $url = json_decode($this->getServerRank(), true);
         
-        if($url['success'] === false){
+        if(!($url['success'] ?? false)){
             $this->getLogger()->critical('https://pmmp.me 에 서버가 등록되어있지 않습니다.');
             $this->getLogger()->notice('https://open.kakao.com/me/nlog 에서 서버를 등록해주세요');
             $this->getServer()->getPluginManager()->disablePlugin($this);
@@ -28,7 +28,7 @@ class McbeRank extends PluginBase implements Listener{
         return Internet::getURL('https://pmmp.me/api/getServer/' . $this->db['ip'] . ':' . $this->db['port']);
     }
     public function onJoin(PlayerJoinEvent $event){
-        $url = yaml_parse($this->getServerRank());
+        $url = json_decode($this->getServerRank(), true);
         $event->getPlayer()->sendMessage('§e§l[ §f서버 §e] §r현재 우리 서버의 순위는 ' . $url['result'] ['rank'] . ' 위 입니다');
     }
 }
