612a207f1141768100a07991758250173f435732
dokidia <42838268+twk1024@users.noreply.github.com>
2020-10-07 04:07:43 +0900

    Update for New McbeRank

--- src/dokidia/McbeRank.php
+++ src/dokidia/McbeRank.php
@@ -16,14 +16,15 @@ class McbeRank extends PluginBase implements Listener{
             'port' => $this->getServer()->getPort()
         ]);
         $this->db = $this->config->getAll();
-        $url = json_decode($this->getServerRank(), true);
-
+        
+            
     }
-    public function getServerRank() : string{
-        return Internet::getURL('http://be.diamc.kr:3500/api/servers' . $this->db['ip'] . ':' . $this->db['port']);
-    }
+    
     public function onJoin(PlayerJoinEvent $event){
-        $url = json_decode($this->getServerRank(), true);
-        $event->getPlayer()->sendMessage('§l§b< §f알림 §b> §r§f현재 우리 서버의 순위는 §6 ' . $url['result'] ['rank'] . ' 위§r§f 입니다! §r(MCBE RANK 기준)');
+        $url = ('http://be.diamc.kr:3500/api/servers' . $this->db['ip'] . ':' . $this->db['port']);
+			$data = (array) json_decode(Internet::getURL($url));
+            $rank = (array) $data['rank'];
+        
+        $event->getPlayer()->sendMessage('§l§b< §f알림 §b> §r§f현재 우리 서버의 순위는 §6($rank)위§r§f 입니다! §r(MCBE RANK 기준)');
     }
 }
