238e2ef0262194a41448864bf092fc2d81427b62
dokidia <42838268+twk1024@users.noreply.github.com>
2020-10-07 03:58:24 +0900

    Update McbeRank.php

--- src/dokidia/McbeRank.php
+++ src/dokidia/McbeRank.php
@@ -19,16 +19,16 @@ class McbeRank extends PluginBase implements Listener{
         $url = json_decode($this->getServerRank(), true);
         
         if(!($url['success'] ?? false)){
-            $this->getLogger()->critical('https://mcbe.cf 에 서버가 등록되어있지 않습니다.');
-            $this->getLogger()->notice('https://mcbe.cf 에서 로그인 후 서버를 등록해주세요');
+            $this->getLogger()->critical('http://be.diamc.kr 에 서버가 등록되어있지 않습니다.');
+            $this->getLogger()->notice('http://be.diamc.kr/new 에서 서버를 등록해주세요');
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
     }
     public function getServerRank() : string{
-        return Internet::getURL('https://api.mcbe.cf/getServer/' . $this->db['ip'] . ':' . $this->db['port']);
+        return Internet::getURL('http://be.diamc.kr:3500/api/servers' . $this->db['ip'] . ':' . $this->db['port']);
     }
     public function onJoin(PlayerJoinEvent $event){
         $url = json_decode($this->getServerRank(), true);
-        $event->getPlayer()->sendMessage('§l§f< §b서버순위 §f> §l§e현재 우리 서버의 순위는§o§a ' . $url['result'] ['rank'] . ' 위§r§l§e 입니다! §r(MCBE RANK 기준)');
+        $event->getPlayer()->sendMessage('§l§b< §f알림 §b> §r§f현재 우리 서버의 순위는 §6 ' . $url['result'] ['rank'] . ' 위§r§f 입니다! §r(MCBE RANK 기준)');
     }
 }
